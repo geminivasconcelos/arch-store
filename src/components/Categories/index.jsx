16eb2c4a04d0d310dd204f39fd711b976a496c7b
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-11-08 19:07:31 -0300

    feat: criacao da categoria

---
+++ src/components/Categories/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+export default function Categories() {
+	return (
+		<div className='navigation-menu'>
+			<h3>Categorias</h3>
+			<ul>
+				<li className="btn-menu"></li>
+				<li className="btn-menu"></li>
+				<li className="btn-menu"></li>
+				<li className="btn-menu"></li>
+				<li className="btn-menu"></li>
+			</ul>
+		</div>
+	);
+};
