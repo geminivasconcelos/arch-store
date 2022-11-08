4a4f31454d1ffc19ee7e5c8e723f39025c9b84d2
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-11-08 19:35:38 -0300

    refactor: ajuste na navegacao das paginas

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -4,13 +4,12 @@ import './style.css'
 export default function Categories() {
 	return (
 		<div className='navigation-menu'>
-			<h3>Categorias</h3>
-			<ul>
-				<li className="btn-menu"></li>
-				<li className="btn-menu"></li>
-				<li className="btn-menu"></li>
-				<li className="btn-menu"></li>
-				<li className="btn-menu"></li>
+			<h3 className='titulo-nav'>Categorias</h3>
+			<ul className='lista-categoria'>
+				<li className="btn-menu">Feminino</li>
+				<li className="btn-menu">Masculino</li>
+				<li className="btn-menu">Infantil</li>
+				<li className="btn-menu">Calçados</li>
 			</ul>
 		</div>
 	);
