162f4f8b780b76f5e36a601ae4f1cd60400bd24f
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-11-08 19:05:23 -0300

    feat: criacao do header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+
+export default function Header() {
+  return (
+	<div className='Header'>
+		<div className="group__header-logo">
+			<h2 className='header__title'>Arch Store</h2>
+			
+		</div>
+
+    </div>	
+	);
+};
