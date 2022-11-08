d11437b3be2ab7fa77acad57eda238651cf6d07a
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-11-08 19:05:38 -0300

    feat: criacao do home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,18 @@
+import Categories from '../../components/Categories';
+import Container from '../../components/Container';
+import Header from '../../components/Header';
+
+
+
+
+export default function Home() {
+    return (
+         <Container>
+            <Header/>
+            <Categories />
+
+
+         </Container>
+
+    );
+};
