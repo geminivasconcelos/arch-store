109f415a8fd3e6d256f56c380b5d3fb93b87bd1a
gevasconcelos <geminivasconceelos@gmail.com>
2022-11-13 16:23:31 -0300

    feat: adicionando link a logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import './style.css'
-
+import React from "react";
+import "./style.css";
 
 export default function Header() {
   return (
-	<div className='Header'>
-		<div className="group__header-logo">
-			<h2 className='header__title'>Arch Store</h2>
-			
-		</div>
-
-    </div>	
-	);
-};
+    <div className="Header">
+      <div className="group__header-logo">
+        <a href="/" className="btn-menuLink">
+          <h2 className="header__title">Arch Store</h2>
+        </a>
+      </div>
+    </div>
+  );
+}
