0b9b64e34b70c18a6ee61866d6b2cb3fdb587241
gevasconcelos <geminivasconceelos@gmail.com>
2022-11-14 19:29:25 -0300

    refactor

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
-import Categories from "../../components/Categories";
+
 import Container from "../../components/Container";
 import Header from "../../components/Header";
+import Categories from "../Categories/index"
 
 export default function Home() {
   return (
